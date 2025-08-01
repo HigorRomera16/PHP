1dae1833e4480a720462b8dd7b45782d02b55276
HigorRomera16 <higor.bagatello@aluno.senai.br>
2025-08-01 14:23:46 -0300

    Create PHP.index.php

---
+++ PHP.index.php
@@ -0,0 +1,31 @@
+<?php
+
+echo "Olá, mundo!!!";
+
+$idade = 25;
+$negativo = -10;
+
+$preco = 19.99;
+$peso = 70.5;
+
+$nome = "Higor";
+$mensagem = 'Olá, mundo!!!';
+
+$verdadeiro = True;
+$falso = False;
+
+echo $idade;
+echo $negativo;
+echo $preco;
+echo $peso;
+echo $nome;
+echo $mensagem;
+echo $verdadeiro;
+echo $falso;
+
+$frutas = array("maça" , "banana" , "laranja");
+
+$idades = array("Maria" => 25, "João" => 30, "Ana" => 22);
+
+
+?>
