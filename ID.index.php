d9416440363c89571a0e4db6f2ecdd2aa0e638ba
HigorRomera16 <higor.bagatello@aluno.senai.br>
2025-08-01 14:34:42 -0300

    Create ID.index.php

---
+++ ID.index.php
@@ -0,0 +1,14 @@
+<?php
+
+$nome = "Higor";
+$idade = 16;
+$altura = 1.85;
+$solteiro = true;
+
+echo "Nome: " . $nome . "<br>";
+echo "Idade: " . $idade . "anos<br>";
+echo "Altura: " . $altura . "metro<br>";
+echo "Solteiro: " . ($solteiro ? "Sim" : "Não") . "<br>";
+
+
+?>
