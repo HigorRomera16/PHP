258ab1124539bf6d31d47015a5048a789dca4513
HigorRomera16 <higor.bagatello@aluno.senai.br>
2025-08-01 14:59:06 -0300

    Create Formulario.html.index.php

---
+++ Formulario.html.index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+	<head>
+		<meta charset="UTF-8">
+		<title>Formulário PHP</title>
+</head>
+<body>
+	<form method="post" action="processa.php">
+		Nome: <input type="text" name="nome"><br>
+		Idade:  <input type="number" name="idade"><br>
+		<input type="submit" value="Enviar">
+</form>
+</body>
+</html>
