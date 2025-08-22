535750ae2a830c56c8d1507ec76477595fa0fe42
HigorRomera16 <higor.bagatello@aluno.senai.br>
2025-08-22 16:42:18 -0300

    Update Aula3.php

--- Aula3.php
+++ Aula3.php
@@ -22,3 +22,73 @@
         ?>
     <body>
 <html>
+
+-------------------------------------------------------------------------------------------------
+
+<html>
+    <head>
+        <title>Contador de Visitas</title>
+        <!--Caracteres latinos(acentos e etc) -->
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <h1>Login no sistema Calu e Barreto SA</h1>
+        <!-- elementos de formulário, como caixas de texto, selects, etc-->
+        <!-- get ele coloca as var na URL. Post não coloca -->
+        <form action="login.php" method="post">
+            <!-- o que estiver no name, será a var do PHP --> 
+            Usuário: <input type="text" name="usuario"> <br>
+            Senha: <input type="password" name="senha"> <br>
+            <input type="submit" value="Login do sistema"> <br>
+        </form>
+        <?php
+          session_start();
+          // inicialmente, o usuário não está logado
+          $_SESSION["logado"] =false;
+          if(isset($_POST["usuario"])){
+            // pega as variáveis do formulário e guarda na sessão
+            $_SESSION["usuario"] = $_POST["usuario"];
+            $_SESSION["senha"] = $_POST["senha"];
+            // verifica usuário e senha
+            if($_SESSION["usuario"] == "admin" && $_SESSION["usuario"] == "senha123") {
+                $_SESSION["logado"] = true;
+                //redirecionar para á página inicial, que é obrigado a estar logado
+                header("location: inicial.php");
+            }else{
+                // se a senha estiver errada
+                print"<h2>Usuário ou senha incorretos!<h2>";
+            }
+            }
+        
+        ?>
+
+    </body>
+</html>
+    
+-------------------------------------------------------------------------------------------------
+
+    <html>
+    <head>
+        <title>Tela Inicial no Sistemas</title>
+        <!--Caracteres latinos(acentos e etc) -->
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <?php
+        session_start();
+        if(isset($_SESSION["logado"])){
+            if($_SESSION["logado"]){
+                // pega o $_SESSION["logado"] da outra tela
+                print "<h1> Bem vindo ".$_SESSION["usuario"]."!</h1>";
+            }else{
+                // se errou usuário ou senha
+                header("location: login3.php");
+            }
+        }else{
+            // se não se logou
+            header("location: login3.php");
+        }
+        ?>
+
+    </body>
+</html>
