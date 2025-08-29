19f58255886ac42646547ddf20397b78451a786b
HigorRomera16 <higor.bagatello@aluno.senai.br>
2025-08-29 16:13:40 -0300

    Create 10desafios.php

---
+++ 10desafios.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="Saudação.php" method="post">
+
+<label>Digite se nome: </label>
+<input type="text" name="Higor">
+ <input type="submit" value="Entrar">
+
+
+
+    </form>
+</body>
+</html>
+
+<?php
+$nomeado = $_POST['Higor'];
+echo "Seja bem vindo! " .$nomeado;
+
+?>
